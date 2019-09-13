383e407d2db2af3256a3f1dc1e4bb2bec5819451
Alexandre Chêne <kooparse@protonmail.com>
2019-09-13 14:31:36 -0400

    Add GenericNumber trait for our math library

--- engine/src/math.rs
+++ engine/src/math.rs
@@ -1,18 +1,29 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 
-pub struct Vector3<N: Add> {
+// Because we can't alias traits yet.
+trait GenericNumber:
+    Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + Sized
+{
+    // Nothing here... it's an "alias".
+}
+
+pub struct Vector3<N: GenericNumber> {
     x: N,
     y: N,
     z: N,
 }
 
-impl<N: Add> Vector3<N> {
+impl<N: GenericNumber> Vector3<N> {
     pub fn new(x: N, y: N, z: N) -> Self {
         Self { x, y, z }
     }
 }
 
-impl<N: Add<Output = N>> Add for Vector3<N> {
+impl<N: GenericNumber> Add for Vector3<N> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
@@ -24,7 +35,7 @@ impl<N: Add<Output = N>> Add for Vector3<N> {
     }
 }
 
-impl<N: Add + Sub<Output = N>> Sub for Vector3<N> {
+impl<N: GenericNumber> Sub for Vector3<N> {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self::Output {
